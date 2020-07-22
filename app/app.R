b6ae634c363b861ac3e497d0cd78de4208e0f83c
larnsce <lars@lse.de>
2020-07-22 13:20:39 +0200

    Add tool 4 to app

--- app/app.R
+++ app/app.R
@@ -28,6 +28,16 @@ performance <- read.xlsx(xlsxFile = here::here("data/performance_sum.xlsx"), she
 biowaste_nutrients_narrow <- read.xlsx(xlsxFile =here::here("data/waste_sum.xlsx"),sheet = 1) %>% 
   gather(7:14,key = parameter,value = value) 
 
+## data for tool 3
+
+# TODO
+
+## data for tool 4
+
+performance_narrow <- read.xlsx(xlsxFile = here::here("data/performance_sum.xlsx"), sheet = 1) %>% 
+  gather(11:16,key = parameter,value = value,na.rm = TRUE)
+
+
 # ui ----------------------------------------------------------------------
 
 ui <- dashboardPage(
@@ -83,23 +93,23 @@ ui <- dashboardPage(
               
       ), # close tab item tool1
       
-      # input tool 1
+      # input tool 2
       tabItem(tabName = "tool2",
               
               # first row
               fluidRow(
                 box(width = 6, title = "Write a title",
                     column(width = 6, checkboxGroupInput(inputId = "Substrate_groups", label = "Substrate groups",
-                                       choices = levels(as.factor(biowaste_nutrients_narrow$Diet_group)),selected = "Food waste")),
+                                                         choices = levels(as.factor(biowaste_nutrients_narrow$Diet_group)),selected = "Food waste")),
                     
                     column(width = 6, checkboxGroupInput(inputId = "Nutrient_parameter",label = "Nutrient parameter",
-                                       choices = levels(as.factor(biowaste_nutrients_narrow$parameter)),selected = "Ash")),
+                                                         choices = levels(as.factor(biowaste_nutrients_narrow$parameter)),selected = "Ash")),
                 ),
                 box(width = 6, title = "Write a title",
                     plotOutput("Boxplot_substrate_groups"),
                 )
               ),
-
+              
               # second row
               fluidRow(
                 box(width = 12, title = "Write a title",
@@ -108,10 +118,12 @@ ui <- dashboardPage(
               )
       ), # close tab item tool2
       
-      # input tool 1
+      # input tool 3
       tabItem(tabName = "tool3",
               fluidRow(
                 box(width = 6, title = "Write a title",
+                    
+                    # TODO: Add tool 3 elements  
                     
                 ),
                 box(width = 6, title = "Write a title",
@@ -120,16 +132,36 @@ ui <- dashboardPage(
               )
       ), # close tab item tool3
       
-      # input tool 1
+      # input tool 4
       tabItem(tabName = "tool4",
+              
+              # first row
               fluidRow(
                 box(width = 6, title = "Write a title",
                     
+                    column(width = 6, checkboxGroupInput(inputId = "Substrate_groups_tool4", label = "Substrate groups", 
+                                                         choices =  levels(as.factor(performance_narrow$Diet_group)), 
+                                                         selected = "Food waste - Household")),
+                    
+                    column(width = 6, checkboxGroupInput(inputId = "Performance_indicator_tool4", label = "Performance indicator", 
+                                                         choices = levels(as.factor(performance_narrow$parameter)), 
+                                                         selected = "Bioconversion_rate_perc_DM"))
+                    
+                ),
+                box(width = 6, title = "Write a title",
+                    plotOutput("Boxplot_substrate_groups_tool4"),
+                    
                 ),
                 
-                box(width = 6, title = "Write a title",
-                    
+                # second row
+                
+                # second row
+                fluidRow(
+                  box(width = 12, title = "Write a title",
+                      dataTableOutput("Performance_indicator_summary_tool4")
+                  )
                 )
+                
               )
       ) # close tab item tool4
     ) # close tab items
@@ -141,17 +173,17 @@ ui <- dashboardPage(
 
 server <- function(input, output) { 
   
-
-# Introduction page -------------------------------------------------------
-
+  
+  # Introduction page -------------------------------------------------------
+  
   output$introtext <- renderText(
     "I suggest you write an introduction text to you app here. Cite relevant papers and add info that\n
   helps people understand how the app works and what the tools are. As you can see, I have no clue how to do that nicely,\n
   but search engines and StackOverflow will help you.")
   
-
-# Tool 1 ------------------------------------------------------------------
-
+  
+  # Tool 1 ------------------------------------------------------------------
+  
   substr_groups <- reactive({ paste(input$Substrate_groups, collapse = ", ") })
   
   # A test to check if the selected checkboxes work correctly
@@ -206,9 +238,9 @@ server <- function(input, output) {
   })
   
   
-
-# Tool 2 ------------------------------------------------------------------
-
+  
+  # Tool 2 ------------------------------------------------------------------
+  
   biowaste_nutrients_narrow_subset <-
     
     reactive({
@@ -255,6 +287,60 @@ server <- function(input, output) {
   # produce summmary table 
   
   output$Nutrient_composition_summary <- renderDT({biowaste_nutrients_stats()})  
+  
+  
+  # Tool 3 ------------------------------------------------------------------
+  
+  # TODO: Add server parts of tool 3
+  
+  # Tool 4 ------------------------------------------------------------------
+  
+  performance_summary_narrow_subset <-
+    
+    reactive({
+      # select Substrate groups based on input
+      performance_narrow %>% 
+        filter(
+          Diet_group %in% input$Substrate_groups_tool4 & 
+            parameter %in% input$Performance_indicator_tool4
+          )
+      
+    })
+  
+  
+  # calculate descriptive statistics
+  
+  performance_narrow_stats <- reactive({
+    
+    performance_summary_narrow_subset() %>% 
+      group_by(Diet_group, parameter) %>% 
+      summarise(n = n(),
+                mean = round(mean(value, na.rm = TRUE), 1),
+                sd = round(mean(value, na.rm = TRUE), 1),
+                median = round(median(value, na.rm = TRUE), 1),
+                max = round(max(value,na.rm = TRUE), 1),
+                min = round(min(value,na.rm = TRUE), 1))
+  })
+  
+  
+  # produce boxplot output based on manipulated data
+  
+  output$Boxplot_substrate_groups_tool4 <- renderPlot({
+    
+    performance_summary_narrow_subset()  %>% 
+      ggplot(aes(Diet_group, value)) +
+      geom_boxplot() +
+      geom_point() +
+      labs(y = "% dm", x="", title = "Biowaste nutrients") +
+      facet_wrap(~parameter) +
+      coord_flip()
+  })
+  
+  
+  # produce summmary table 
+  
+  output$Performance_indicator_summary_tool4 <- renderDT({ performance_narrow_stats() })
+  
 }
 
 
